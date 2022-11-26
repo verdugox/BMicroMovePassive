d62c0d6aed69fcf7bdd4711ffbb6830d2e9258ef
Luis Acuña <luisandresverdugox@gmail.com>
2022-11-25 19:19:45 -0500

    Subiendo primer código de movimiento Passivo

---
+++ MovePassive/src/main/java/com/MovePassive/MovePassive/MovePassiveApplication.java
@@ -0,0 +1,13 @@
+package com.MovePassive.MovePassive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovePassiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovePassiveApplication.class, args);
+	}
+
+}
